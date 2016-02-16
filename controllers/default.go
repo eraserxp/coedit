131dccc093119ab570bfd1f07d5c5d1dddfa822a
xujn <xujn238@gmail.com>
2016-02-15 17:17:47 -0800

    modify controlor

--- controllers/default.go
+++ controllers/default.go
@@ -2,14 +2,23 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"fmt"
 )
 
 type MainController struct {
 	beego.Controller
 }
 
+//func (c *MainController) Get() {
+//	c.Data["Website"] = "beego.me"
+//	c.Data["Email"] = "astaxie@gmail.com"
+//	c.TplName = "index.tpl"
+//}
+
 func (c *MainController) Get() {
+	fmt.Print("123")
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	c.TplName = "index2.tpl"
+
 }
