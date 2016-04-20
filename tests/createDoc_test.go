625ea0804faca44e7101d3d7f20c390b34ab143e
xujn <xujn238@gmail.com>
2016-04-19 21:17:19 -0700

    test createDoc

---
+++ tests/createDoc_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/eraserxp/coedit/models"
+	"github.com/astaxie/beego/orm"
+	"fmt"
+)
+var o  orm.Ormer = orm.NewOrm()
+func TestCreateDoc(t *testing.T) {
+
+	docNew := &models.Documents{"testid","test_content","D","test@gmail"}
+	// Only pass t into top-level Convey calls
+	err := docNew.Save()
+	if (err == nil) {
+		fmt.Println("***************************************")
+	}
+	Convey("Test Create Doc in database", t, func() {
+		var lists []orm.ParamsList
+
+		num, _ := o.Raw(" select Id from documents where id = ?", "testid").ValuesList( &lists)
+
+		num=num+1
+		So(lists[0][0], ShouldEqual, "testid")
+	})
+}
